d2091fdbf193e871c8ad7f6ca7799f6a8d4c048a
ansthsys <azmiythufail91@gmail.com>
2023-03-02 15:57:49 +0700

    assessment day 9

---
+++ ASSESSMENT/ASSESSMENT-DAY9/2023_02_21_082602_create_purchase_order_lines_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseOrderLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->integer('qty');
+            $table->float('price');
+            $table->float('dicount');
+            $table->float('total');
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_order_lines');
+    }
+}

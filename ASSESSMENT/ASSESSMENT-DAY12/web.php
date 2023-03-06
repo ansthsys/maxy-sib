223af8dc1afa4c412173d3abe1ecce5ddbe1837b
ansthsys <azmiythufail91@gmail.com>
2023-03-06 19:13:10 +0700

    Create assessment for day 12

---
+++ ASSESSMENT/ASSESSMENT-DAY12/web.php
@@ -0,0 +1,143 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'penjualan'], function () use ($router) {
+    $router->get('/', function () {
+        return response()->json([
+            'message' => 'Get all list penjualan',
+            'error' => false,
+            'data' => [
+                [
+                    "id" => 1,
+                    "nomor" => "SALE/00001",
+                    "customer" => "Joko"
+                ],
+                [
+                    "id" => 2,
+                    "nomor" => "SALE/00002",
+                    "customer" => "Asep"
+                ],
+                [
+                    "id" => 3,
+                    "nomor" => "SALE/00003",
+                    "customer" => "Dadang"
+                ],
+                [
+                    "id" => 4,
+                    "nomor" => "SALE/00004",
+                    "customer" => "Udin"
+                ],
+                [
+                    "id" => 5,
+                    "nomor" => "SALE/00005",
+                    "customer" => "Amang"
+                ],
+            ]
+        ]);
+    });
+
+    $router->get('/{id}', function ($id) {
+        return response()->json([
+            'message' => "Get detail penjualan id: $id",
+            'error' => false,
+            'data' => [
+                "id" => 1,
+                "nomor" => "SALE/00001",
+                "customer" => "Joko",
+                "alamat" => "Jl. Jakarta",
+                "total" => 20000000
+            ]
+        ]);
+    });
+
+    $router->post('/', function (Request $req) {
+        return response()->json([
+            'message' => "Success input data",
+            'error' => false,
+            'data' => [
+                "nomor" => $req->nomor,
+                "customer" => $req->customer,
+                "alamat" => $req->alamat,
+                "total" => $req->total
+            ]
+        ]);
+    });
+
+    $router->put('/{id}', function (Request $req, $id) {
+        return response()->json([
+            'message' => "Success update data id: $id",
+            'error' => false,
+            'data' => [
+                "id" => $id,
+                "nomor" => $req->nomor,
+                "customer" => $req->customer,
+                "alamat" => $req->alamat,
+                "total" => $req->total
+            ]
+        ]);
+    });
+
+    $router->delete('/{id}', function ($id) {
+        return response()->json([
+            'message' => "Success delete data id: $id",
+            'error' => false,
+        ]);
+    });
+
+    $router->get('/{id}/confirm', function (Request $req, $id) {
+        $user = $req->user();
+
+        if ($user == null) {
+            return response()->json([
+                "message" => "Unauthorize",
+                "error" => true,
+                "user" => $user,
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => "Success confirmed",
+            'error' => false,
+            'user' => $user,
+        ]);
+    });
+
+    $router->post('/{id}/sendmail', function (Request $req, $id) {
+        $body = $req->body;
+
+        if (!$body) {
+            return response()->json([
+                'message' => "Email body is required",
+                'error' => true,
+            ], 400);
+        }
+
+        Mail::raw("$body", function ($message) {
+            $message->to("azmiythufail91@gmail.com")
+                ->subject('Lumen - Maxy Academy');
+        });
+
+        return response()->json([
+            'message' => "Email sent successfully to id: $id",
+            'error' => false,
+        ]);
+    });
+});
